f1681468adea1c522d449d2cb142cffb1453e419
Gisan999 <ruheatheking3@gmail.com>
2023-11-05 03:19:32 +0600

    completing the Subscribe to our Newsletter section with all device responsive and show success message

--- src/Components/Home/SubscribeSection.jsx
+++ src/Components/Home/SubscribeSection.jsx
@@ -1,5 +1,24 @@
+import Swal from 'sweetalert2';
 import bg from '../../assets/bg_3.jpg'
 const SubscribeSection = () => {
+
+    const handleSubscribe = (e) => {
+        e.preventDefault();
+        const email = e.target.email;
+        // console.log(email);
+        // let input1 = document.getElementById('input1');
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Thank you for subscribing to our  newsletter',
+            showConfirmButton: false,
+            timer: 1500
+        })
+        
+        // input1.value = "";
+        email.value = '';
+    }
+
     return (
         <div style={{
             backgroundImage: `url(${bg})`,
@@ -7,16 +26,22 @@ const SubscribeSection = () => {
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'right'
         }}>
-            <div className='flex flex-col md:flex-row justify-center lg:gap-24 py-24 px-5 lg:px-0'>
+            <div className='flex flex-col md:flex-row justify-center lg:gap-24 py-32 px-5 lg:px-0'>
                 <div><h2 className='text-5xl font-bold font-mono'>Subscribe to our <br /> Newsletter</h2></div>
                 <div>
-                <div className='flex py-6'>
-                                <input className='bg-gray-50 lg:pr-24 px-5 rounded-md border-4 border-red-300' placeholder='Email Address' type="email" name="email" id="" />
-                                <button className='bg-sky-400 px-5 border-4 border-red-400  rounded-r-lg relative right-8 text-white font-bold'>Subscribe</button>
-                            </div>
+                    <form name='contact-form' onSubmit={handleSubscribe}>
+                        <div className='flex py-6'>
+
+                            <input  className='bg-gray-50 lg:pr-24 px-5 rounded-md border-4 border-red-300' placeholder='Email Address' required type="email" name="email" id="input1" />
+                          
+                            <input   className='bg-sky-400 px-5 border-4 border-red-400  rounded-r-lg relative right-8 text-white font-bold' type="submit" value="Subscribe" />
+                        </div>
+                        
+                    </form>
+
                 </div>
             </div>
-            
+
         </div>
     );
 };
